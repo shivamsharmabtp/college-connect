ca75106e06ce41155f3f563fa3367d9f0b46546a
shivam <shivamsharma.btp@gmail.com>
2023-03-16 17:08:01 -0400

    implementing token logic

--- college-connect/models/userModel.js
+++ college-connect/models/userModel.js
@@ -61,6 +61,37 @@ var UserSchema = new mongoose.Schema({
 });
 
 
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+
 var User = mongoose.model('User', UserSchema);
 
 
