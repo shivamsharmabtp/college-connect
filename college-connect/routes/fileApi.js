5c15e334143e909383681af3d8b55ca5b64b2f3f
shivam <shivamsharma.btp@gmail.com>
2023-03-28 15:21:18 -0400

    Search files API

--- college-connect/routes/fileApi.js
+++ college-connect/routes/fileApi.js
@@ -91,6 +91,77 @@ router.post("/filelist", (req, res) => {
 });
 
 
+router.post("/search", (req, res) => {
+  try {
+    body = JSON.parse(req.body.data);
+    File.find(
+      { $text: { $search: body.query } },
+      { score: { $meta: "textScore" } }
+    )
+      .skip(0)
+      .limit(20)
+      .sort({ score: { $meta: "textScore" } })
+      .exec(async function (err, doc) {
+        var files = [];
+        for (var i = 0; i < doc.length; i++) {
+          if (doc[i].name == body.query && body.source == "read-main") continue;
+          file = {
+            name: doc[i].name,
+            fileId: doc[i].uniqueId,
+            views: await View.countDocuments({
+              pageType: "read-main",
+              pageId: doc[i].uniqueId,
+            }),
+            time: doc[i].uploadTime,
+            author: doc[i].author,
+            authorName: await username(doc[i].author),
+            pathData: await Course.findOne({ _id: doc[i].courseIds[0] }).exec(),
+            semester: doc[i].semester,
+            department: doc[i].department,
+            university: doc[i].university,
+            description: doc[i].description,
+          };
+          files.push(file);
+        }
+        let moreDoc = await File.find(
+          { name: new RegExp(body.query, "i") },
+          null,
+          { limit: 15 - doc.length }
+        ).exec();
+        doc = moreDoc;
+        for (i = 0; i < moreDoc.length; i++) {
+          if (moreDoc[i].name == body.query && body.source == "read-main")
+            continue;
+          file = {
+            name: doc[i].name,
+            fileId: doc[i].uniqueId,
+            views: await View.countDocuments({
+              pageType: "read-main",
+              pageId: doc[i].uniqueId,
+            }),
+            time: doc[i].uploadTime,
+            author: doc[i].author,
+            authorName: await username(doc[i].author),
+            pathData: await Course.findOne({ _id: doc[i].courseIds[0] }).exec(),
+            semester: doc[i].semester,
+            department: doc[i].department,
+            university: doc[i].university,
+            description: doc[i].description,
+          };
+          if (!files.find((f) => f.fileId == file.fileId)) files.push(file);
+        }
+
+        if (files.length == 0 && false) {
+          notifySlack("No result found for " + body.query, "search-alerts");
+        }
+        res.json(files);
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error proccesing request.");
+  }
+});
+
 function username(id) {
   return new Promise((resolve, reject) => {
     User.findById(ObjectId(id), function (err, user) {
