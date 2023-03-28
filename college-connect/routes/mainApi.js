ef52b617a01687852b22d490eca00374f9fe8e93
shivam <shivamsharma.btp@gmail.com>
2023-03-28 15:35:18 -0400

    completing search page route completing styling and script of search page

--- college-connect/routes/mainApi.js
+++ college-connect/routes/mainApi.js
@@ -4,6 +4,18 @@ module.exports = router;
 
 const fileName = __filename;
 
+router.get('/search', (req, res) => {
+  try{
+  var li = req.cookies.li
+  res.render('search', {
+      li : li
+  });
+} catch (error) {
+  console.log(error);
+  res.status(500).send('Error proccesing request.');
+}
+})
+
 /* Homepage */
 router.get("/", (req, res) => {
   try {
@@ -11,29 +23,5 @@ router.get("/", (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send("Error proccesing request.");
-    var err = error;
-    if (err) {
-      var apiName = req.method + " " + req.originalUrl;
-      if (typeof err == "object") {
-        var errMsg =
-          "*File : " +
-          fileName +
-          "*\n*API : " +
-          apiName +
-          "*\nError - ```" +
-          JSON.stringify(err, Object.getOwnPropertyNames(err), 2) +
-          "```";
-      } else {
-        var errMsg =
-          "*File : " +
-          fileName +
-          "*\n*API : " +
-          apiName +
-          "*\nError - ```" +
-          err +
-          "```";
-      }
-      console.log(errMsg);
-    }
   }
 });
